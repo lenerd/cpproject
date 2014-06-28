93dab7668ae7bb19f23aae2bd8c52dd0fe6cbb45
Lennart Braun <lenerd@posteo.de>
2014-06-29 01:37:47 +0200

    added some documentation

--- src/subdirectory/lib.h
+++ src/subdirectory/lib.h
@@ -1,9 +1,30 @@
 #ifndef LIB_H
 #define LIB_H
 
+/**
+ * \file
+ * \brief An example.
+ * \author Lennart Braun <lenerd@posteo.de>
+ * 
+ * An extended description.
+ */
 
+
+/**
+ * \fn bool say_true (void)
+ * \brief Returns true.
+ *
+ *  Returns the boolean value true.
+ *  Really.
+ */
 bool say_true (void);
 
+/**
+ *  \brief Returns false.
+ *
+ *  Returns the boolean value false.
+ *  Really.
+ */
 bool say_false (void);
 
 
